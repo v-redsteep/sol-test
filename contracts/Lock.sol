12143a0902c87f106eabdc3b4870bbfe45208fd7
v.simanov <v.simanov@redsteep.com>
2023-01-16 10:11:02 +0300

    test change

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -20,6 +20,8 @@ contract Lock {
         owner = payable(msg.sender);
     }
 
+
+
     function withdraw() public {
         // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
         // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
